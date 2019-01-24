3d803280426342e3a3b97d73b838a492bc85a67d
Andy Prevalsky <andyprevalsky@yahoo.com>
2019-01-24 16:07:21 -0600

    First Quiz

---
+++ Quiz-1/HelloWorld.java
@@ -0,0 +1,21 @@
+// Quiz 1
+/*
+ * EE422C Quiz 1 submission by
+ * Andy Prevalsky
+ * agp952
+ * 16200
+ * Spring 2019
+ */
+ 
+public class HelloWorld {
+    /**
+        Has two functions, one the prints hello world
+        and another that prints any string passed as input
+     */
+    public static void hello() {
+        System.out.println("Hello World");
+    }
+    public static void printMessage(String in) {
+        System.out.println(in);
+    }
+};
---
+++ Quiz-1/Main.java
@@ -0,0 +1,17 @@
+// Quiz 1
+/*
+ * EE422C Quiz 1 submission by
+ * Andy Prevalsky
+ * agp952
+ * 16200
+ * Spring 2019
+ */
+public class Main {
+    /**
+        main function that runs the hello world program
+     */
+    public static void main(String[] args) {
+        HelloWorld.hello();
+        HelloWorld.printMessage("My own String");
+    }
+};
